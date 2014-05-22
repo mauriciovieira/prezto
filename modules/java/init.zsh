7a2810c8703ec36a8dc4acdbb63b3df3e047d725
Mauricio Vieira <mauricio@mauriciovieira.net>
2014-05-22 10:29:35 -0300

    Module for java.

---
+++ modules/java/init.zsh
@@ -0,0 +1,16 @@
+#
+# Sets java
+#
+# Authors:
+#   Mauricio Vieira <mauricio.vieira@gmail.com>
+#
+
+# Return if requirements are not found.
+if (( ! $+commands[java] )); then
+  return 1
+fi
+
+# set JAVA_HOME into the shell session.
+if [[ -n $(/usr/libexec/java_home) ]]; then
+  export JAVA_HOME=$(/usr/libexec/java_home)
+fi
